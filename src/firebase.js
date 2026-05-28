8907dae797eba0cca63a431681e5499f63bc5795
haldaneholm-hash <haldane.holm@gmail.com>
2026-05-28 19:00:16 +0200

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbW0a__twtENr5J7BhT6YpBblCqZ4wmXM",
+  authDomain: "locateit-invoices.firebaseapp.com",
+  projectId: "locateit-invoices",
+  storageBucket: "locateit-invoices.firebasestorage.app",
+  messagingSenderId: "945002350239",
+  appId: "1:945002350239:web:0be37aae77c121fa6444aa",
+  measurementId: "G-VE9FMN2Y89"
+};
+
+export const app = initializeApp(firebaseConfig);
